1b1ac599874695d762092cfaca8f5ad94fc71d7b
chinpeerapat <peerapat.chiaprasert@gmail.com>
2024-06-29 10:20:49 +0700

    update BE

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://clerk.workwhales.com",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
